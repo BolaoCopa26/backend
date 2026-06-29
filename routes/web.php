61a347fd05ce0e56ec476e5f450d411a2a1c35e2
axelrammon7 <axel.work7@gmail.com>
2026-06-29 10:32:14 -0300

    feat: add /setup-db route to manually run migrations and debug connection

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/setup-db', function () {
+    try {
+        \Illuminate\Support\Facades\DB::connection()->getPdo();
+        $output = "DB connected!\n";
+        
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        $output .= \Illuminate\Support\Facades\Artisan::output();
+        
+        \Illuminate\Support\Facades\Artisan::call('db:seed', ['--force' => true]);
+        $output .= "\nSeeders: " . \Illuminate\Support\Facades\Artisan::output();
+        
+        return "<pre>" . $output . "</pre>";
+    } catch (\Exception $e) {
+        return "<pre>DB Error: " . $e->getMessage() . "\n\nPlease check your Render Environment Variables (DB_HOST, DB_PORT=4000, DB_USERNAME, etc).</pre>";
+    }
+});
